4252784a55d3d39eb280f971be00bf531dd8cb3a
icyethics <97385843+icyethics@users.noreply.github.com>
2025-02-26 12:14:48 +0100

    Cleaned up repository

--- items/Jokers/blade_1.lua
+++ items/Jokers/blade_1.lua
@@ -45,6 +45,7 @@ SMODS.Joker {
             G.jokers.cards[_my_pos + 1].getting_sliced and not
             G.jokers.cards[_my_pos + 1].ability.eternal then
                 if G.jokers.cards[_my_pos + 1].config.center.is_vampire or G.jokers.cards[_my_pos + 1].config.center.key == "j_vampire" then
+                    if G.jokers.cards[_my_pos + 1].config.center.is_vampire or G.jokers.cards[_my_pos + 1].config.center.key == "j_vampire" then
 
                         
                         -- steals x_mult
